a142d71f9dca5cd9f2b98d1fb919e598117d542b
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-08-14 00:08:46 +0600

    installed tailwindCSS properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import './App.css';
+import "./App.css";
 
-function App() {
-
-  return (
-    <>
-      <div>
-        <h1>Blood Buddy</h1>
-      </div>
-    </>
-  )
-}
+const App = () => {
+	return (
+		<>
+			<div>
+				<h1 className="animate-bounce font-bold text-3xl">
+					Blood Buddy
+				</h1>
+			</div>
+		</>
+	);
+};
 
 export default App;
